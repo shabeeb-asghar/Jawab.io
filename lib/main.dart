f226349b719891ad8a244143e9659964726b5336
Eman Khalid <112969267+emkay3002@users.noreply.github.com>
2024-09-18 01:04:35 +0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:jawab/screens/startup_screens.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Jawab.io',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const StartupScreen(),
     );
   }
 }
