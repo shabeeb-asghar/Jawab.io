81938c8c01d210ee5396c3b7b7c8f3975d932117
emkay3002 <emankhalid2003@gmail.com>
2024-09-18 18:33:54 +0500

    onboarding screen 3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jawab_io/screens/Onboarding_Screens/onboarding_1.dart';
 import 'package:jawab_io/screens/Onboarding_Screens/onboarding_2.dart';
+import 'package:jawab_io/screens/Onboarding_Screens/onboarding_3.dart';
 import 'package:jawab_io/screens/Onboarding_Screens/onboarding_4.dart';
 
 void main() {
@@ -13,8 +14,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: OnBoarding_4()
-        );
+        debugShowCheckedModeBanner: false, home: OnBoarding_3());
   }
 }
--- lib/screens/Onboarding_Screens/onboarding_4.dart
+++ lib/screens/Onboarding_Screens/onboarding_4.dart
@@ -3,56 +3,80 @@ import 'package:jawab_io/Widgets/Button.dart';
 import 'package:jawab_io/Widgets/Dots.dart';
 import 'package:jawab_io/Widgets/Image_Container.dart';
 
-void Press_Skip_Button()
-{
+void Press_Skip_Button() {}
 
-}
+void Press_Next_Button() {}
 
-void Press_Next_Button()
-{
-
-}
-
-class OnBoarding_4 extends StatelessWidget{
+class OnBoarding_4 extends StatelessWidget {
   const OnBoarding_4({super.key});
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SafeArea(child: 
-      Column(crossAxisAlignment: CrossAxisAlignment.center,
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-        Row(
-          children: [
-            SizedBox(width: 290,),
-            Button("Skip",16,Colors.white,FontWeight.w300,Color.fromARGB(255,36,22,73),20,80,Press_Skip_Button),
-          ],
-        ),
-        SizedBox(height: 20,),
-        Image_Container(Image_Path: "Assets/Images/4.png",Y_Index_Shadow: 25, X_Index_Image: 78,Y_Index_Image: 65, Image_Height: 180, Image_Width: 180,),
-        SizedBox(height: 30,),
-        SizedBox(width: 335,child: Text("Audio / Video Transcription",textAlign: TextAlign.center,style: TextStyle(
-          fontFamily: "Poppins",
-          fontSize: 28,
-          fontWeight: FontWeight.w700,
-          color: Colors.black
-          
-        ),),),
-        SizedBox(height: 30,),
-        SizedBox(width:300,child: Text("Transcribe audio and video content, including YouTube links, into Arabic text.",textAlign: TextAlign.center,style: TextStyle(
-          color: Color.fromARGB(255, 136, 136, 136),
-          fontFamily: "Poppins",
-          fontSize: 12,
-          fontWeight: FontWeight.w300
-        ),)),
-        SizedBox(height: 30,),
-        Button("Next", 16, Colors.white, FontWeight.w600, Color.fromARGB(255,36,22,73), 50, 335,Press_Next_Button),
-        SizedBox(height: 20),
-        Padding(
-          padding: EdgeInsets.only(left: 125),
-          child: Dots(3),
-        )
-      ],)
-      ),
+          Row(
+            children: [
+              SizedBox(
+                width: 290,
+              ),
+              Button("Skip", 16, Colors.white, FontWeight.w300,
+                  Color.fromARGB(255, 36, 22, 73), 20, 80, Press_Skip_Button),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Image_Container(
+            Image_Path: "Assets/Images/4.png",
+            Y_Index_Shadow: 25,
+            X_Index_Image: 78,
+            Y_Index_Image: 65,
+            Image_Height: 180,
+            Image_Width: 180,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            width: 335,
+            child: Text(
+              "Audio / Video Transcription",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontSize: 28,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+              width: 300,
+              child: Text(
+                "Transcribe audio and video content, including YouTube links, into Arabic text.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromARGB(255, 136, 136, 136),
+                    fontFamily: "Poppins",
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300),
+              )),
+          SizedBox(
+            height: 30,
+          ),
+          Button("Next", 16, Colors.white, FontWeight.w600,
+              Color.fromARGB(255, 36, 22, 73), 50, 335, Press_Next_Button),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.only(left: 125),
+            child: Dots(3),
+          )
+        ],
+      )),
     );
   }
 }
