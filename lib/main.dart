bf14c0696d308a4e0d3ad6fa50c4ebd767121e85
Shabeeb-Asghar <shabeebasghar57@gmail.com>
2024-09-18 13:16:23 +0500

    Onboarding 9

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:jawab_io/screens/Onboarding_Screens/onboarding_1.dart';
 import 'package:jawab_io/screens/Onboarding_Screens/onboarding_2.dart';
 import 'package:jawab_io/screens/Onboarding_Screens/onboarding_4.dart';
+import 'package:jawab_io/screens/Onboarding_Screens/onboarding_9.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnBoarding_4()
+      home: OnBoarding_9()
         );
   }
 }
